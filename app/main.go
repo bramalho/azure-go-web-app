802d089c2f85142166094d5626ec36e7444bafff
Bruno Ramalho <bfmramalho@gmail.com>
2020-02-29 09:39:09 +0100

    app

--- app/main.go
+++ app/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"status":  "ok",
+		"message": "It works!",
+	})
+}
+
+func pingHandler(w http.ResponseWriter, _ *http.Request) {
+	t := time.Now()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"status":  "ok",
+		"message": fmt.Sprint("Pong - ", t.Format("2006-01-02 15:04:05")),
+	})
+}
 
 func main() {
-	fmt.Println("It works!")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler).Methods("GET")
+	router.HandleFunc("/ping", pingHandler).Methods("GET")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	log.Println("Listning at http://localhost:" + port)
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
